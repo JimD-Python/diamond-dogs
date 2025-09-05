c5a69b0ad6c77901af96496dd26f881e2f2a48b0
Jim Donachie <jim.donachie@btinternet.com>
2025-09-05 15:12:11 +0100

    first commit

---
+++ terraform.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>5.0"
+    }
+  }
+  
+   cloud {
+    organization = "globomantics-jimd"
+
+    workspaces {
+      name = "diamonddogs-app-useast1-dev" 
+    }
+
+  }
+}
